89115b2b2857591368f7fc33282b26cb7908fd94
Illia Pauliushchyk <ilyapavlushchik221@gmail.com>
2023-11-16 12:48:52 +0300

    ss

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const groupUsernameText = document.getElementById('group_username');
 const tg = window.Telegram.WebApp;
 tg.expand();
 
+//
+
 groupIdText.innerHTML = JSON.stringify(tg.initDataUnsafe.user);
 groupUsernameText.innerHTML = JSON.stringify(tg.initDataUnsafe.chat);
 groupDataText.innerHTML = JSON.stringify(tg);
