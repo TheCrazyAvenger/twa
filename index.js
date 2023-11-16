d02ffb621014e88583080cac4f0cc14c4a1127de
Illia Pauliushchyk <ilyapavlushchik221@gmail.com>
2023-11-16 11:33:33 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const groupNameText = document.getElementById('group_name');
 const groupIdText = document.getElementById('group_id');
 const groupUsernameText = document.getElementById('group_username');
 
-groupNameText.textContent = ' ' + window.Telegram.WebApp.initDataUnsafe;
+groupNameText.textContent = JSON.stringify(
+  window.Telegram.WebApp.initDataUnsafe
+);
 
 groupNameText.textContent = ' ' + window.Telegram.WebApp.initDataUnsafe.chat.id;
 groupUsernameText.textContent =
